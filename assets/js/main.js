d297ff0c5414da71dc34542ab33d520bd8784a86
pollohultima <matteo.polonio@gmail.com>
2021-10-18 18:14:23 +0200

    9/9 completed
    going to separeted folder

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,7 +9,7 @@ STRUMENTI
 -querySelector
 */
 
-/* let firstNumber = parseInt(prompt('Inserisci il primo numero'));
+let firstNumber = parseInt(prompt('Inserisci il primo numero'));
 console.log(firstNumber);
 
 let secondNumber = parseInt(prompt('Inserisci il secondo numero'));
@@ -22,7 +22,7 @@ if (firstNumber > secondNumber) {
     document.querySelector('.snack_1').innerHTML = 'Il numero maggiore è il secondo: ' + secondNumber;
 } else {
     document.querySelector('.snack_1').innerHTML = 'I numeri selezionati sono pari';
-} */
+}
 
 
 
@@ -37,7 +37,7 @@ STRUMENTI
 -querySelector
 */
 
-/* let firstWord = prompt('Inserisci la prima parola')
+let firstWord = prompt('Inserisci la prima parola')
 console.log(firstWord.length);
 
 let secondWord = prompt('Inserisci la seconda parola')
@@ -51,7 +51,7 @@ if (firstWord.length > secondWord.length) {
     document.querySelector('.snack_2long').innerHTML = 'La parole più lunga è: ' + secondWord;
 } else {
     document.querySelector('.snack_2').innerHTML = 'La parole hanno la stessa lunghezza';
-} */
+}
 
 
 
@@ -67,13 +67,13 @@ STRUMENTI
 */
 
 //Dichiaro la variabile somma fuori dal ciclo per farci riferimento anche al di fuori del ciclo
-/* let sommaNum = 0;
+let sommaNum = 0;
 
 for (let index = 0; index < 10; index++) {
     const userNumber = parseInt(prompt('Inserisci un numero'));
     sommaNum += userNumber;
 }
-document.querySelector('.snack_3').innerHTML = 'La somma dei numeri inseriti è: ' + sommaNum; */
+document.querySelector('.snack_3').innerHTML = 'La somma dei numeri inseriti è: ' + sommaNum;
 
 
 
@@ -82,7 +82,7 @@ In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby,
 chiedi all’utente il suo nome e comunicagli se può partecipare o no alla festa.
 */
 
-/* const userName = prompt('Inserisci la tuo nome');
+const userName = prompt('Inserisci la tuo nome');
 const guestList = ['Matteo', 'Gennaro', 'Enrico', 'Astrid', 'Joshua'];
 
 
@@ -92,12 +92,11 @@ if (guestList.indexOf(userName) >= 0) {
 } else {
     document.querySelector('.snack_4').innerHTML = 'Spiacente ma non sei nella lista, alza i tacchi';
 }
- */
 
 
 
 /* Snack 5: Crea un array vuoto. Chiedi per 6 volte all’utente di inserire un numero, se è dispari inseriscilo nell’array. */
-/* let array = [];
+let array = [];
 for (let index = 0; index < 6; index++) {
     const userNumber = parseInt(prompt('Inserisci un numero'));
     if (userNumber % 2 != 0) {
@@ -106,4 +105,49 @@ for (let index = 0; index < 6; index++) {
 
     }
 }
-document.querySelector('.snack_5').innerHTML = 'Nel tuo array ci sono i numeri dispari: ' + array; */
+document.querySelector('.snack_5').innerHTML = 'Nel tuo array ci sono i numeri dispari: ' + array;
+
+/*
+Snack 6: Stampa il cubo dei primi N numeri,
+dove N è un numero indicato dall’utente.
+ */
+const userElevated = []
+const userNum = parseInt(prompt('Inserisci un numero'))
+
+for (let index = 1; index < userNum; index++) {
+    let elevated = Math.pow(index, 3)
+    userElevated.push(elevated)
+}
+document.querySelector('.snack_6').innerHTML = 'I cubi dei numeri fra 0 e ' + userNum + ' sono: ' + userElevated;
+
+
+
+
+/*
+Snack 7: Stampa le potenze di 2 fino a 1000.
+*/
+for (let index = 1; index < 10; index++) {
+    let powa = Math.pow(2, index);
+    console.log(powa);
+}
+
+/* Snack 8: Chiedi un numero di 4 cifre all’utente
+e calcola la somma di tutte le cifre che compongono il numero.
+ */
+const userNumbers = prompt('Inserisci un numero di 4 cifre');
+
+let somDigit = 0
+for (let index = 0; index < 4; index++) {
+    const digit = parseInt(userNumbers.charAt(index));
+    somDigit += digit;
+}
+document.querySelector('.snack_8').innerHTML = 'La somma dei numeri digitati è: ' + somDigit;
+
+
+
+/* Snack 9: Calcola la somma e la media dei primi 10 numeri.
+ */
+const somma = 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10;
+document.querySelector('.snack_9').innerHTML = 'La somma dei primi 10 numeri è ' + somma;
+const media = somma / 10
+document.querySelector('.snack_9').innerHTML = 'La somma dei primi 10 numeri è ' + media;
